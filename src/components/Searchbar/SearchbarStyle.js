25e2e6a32cd936c602c7073b48ce91e42e9612bc
murat güneş <mgunes004@gmail.com>
2021-11-10 20:52:31 +0300

    first commit

---
+++ src/components/Searchbar/SearchbarStyle.js
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    backgroundColor: '#d6cdcd',
+    padding: 2,
+    margin: 5,
+    borderRadius: 20,
+  },
+});
